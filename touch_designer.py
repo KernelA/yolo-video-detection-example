e7d3f5024ebcb40394231d42744d6c253ccdc690
KernelA <17554646+KernelA@users.noreply.github.com>
2023-09-05 13:02:41 +0300

    New parameters added :minor:
    - Move default logging config to package
    - Draw optional text and score
    - Process upto max image size

--- touch_designer.py
+++ touch_designer.py
@@ -1,15 +1,16 @@
+import time
 from multiprocessing import shared_memory
 
-import numpy as np
 import cv2
-
-from yolo_models.processing.info import BufferStates, States, ParamsIndex
+import numpy as np
+from yolo_models.processing.info import (BufferStates, DrawInfo, ParamsIndex,
+                                         States)
 
 # Sync with external process
 SHARED_MEM_PARAMS_LIST = shared_memory.ShareableList(name="params")
 
-WIDTH = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_WIDTH]
-HEIGHT = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_HEIGHT]
+MAX_IMAGE_WIDTH = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_WIDTH]
+MAX_IMAGE_HEIGHT = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_HEIGHT]
 DTYPE = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_DTYPE]
 NUM_CHANNELS = SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_CHANNELS]
 EXIT = False
@@ -21,28 +22,33 @@ SHARED_MEM_UPDATE_STATES = shared_memory.SharedMemory(
 SHARED_MEM_ARRAY = shared_memory.SharedMemory(
     name=SHARED_MEM_PARAMS_LIST[ParamsIndex.SHARED_ARRAY_MEM_NAME], create=False)
 
-ARRAY = np.ndarray((WIDTH, HEIGHT, NUM_CHANNELS), dtype=DTYPE, buffer=SHARED_MEM_ARRAY.buf)
+ARRAY = np.ndarray((MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT, NUM_CHANNELS),
+                   dtype=DTYPE, buffer=SHARED_MEM_ARRAY.buf)
 
 
 def onSetupParameters(scriptOp):
     page = scriptOp.appendCustomPage("Detection")
     p = page.appendFloat("Nms", label="Intersection ratio between bboxes")
-    p.default = 0.5
     p.min = 0
     p.max = 1
-    p = page.appendFloat("Score", label="Minimum score for object")
     p.default = 0.5
+    p = page.appendFloat("Score", label="Minimum score for object")
     p.min = 0.1
     p.max = 1
+    p.default = 0.5
     p = page.appendInt("Maxk", label="Maximum number of objects to detect based on score")
-    p.default = 5
     p.min = 0
     p.max = 1000
+    p.default = 5
     p = page.appendFloat("Eta", label="Filtering")
-    p.default = 1.0
     p.min = 0
     p.clampMin = True
     p.max = 1000
+    p.default = 1.0
+    p = page.appendToggle("Drawtext", label="Draw text labels")
+    p.default = False
+    p = page.appendToggle("Drawscore", label="Draw score labels")
+    p.default = False
     return
 
 
@@ -79,23 +85,49 @@ def onCook(scriptOp):
     if COLOR_CONVERSION is not None:
         image = cv2.cvtColor(frame, COLOR_CONVERSION)
 
-    image = cv2.resize(image, (WIDTH, HEIGHT))
+    if image.shape[0] > MAX_IMAGE_HEIGHT:
+        debug("Too large image height")
+        scriptOp.clear()
+        return
 
+    if image.shape[1] > MAX_IMAGE_WIDTH:
+        debug("Too large image width")
+        scriptOp.clear()
+        return
+
+    SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_HEIGHT] = image.shape[0]
+    SHARED_MEM_PARAMS_LIST[ParamsIndex.IMAGE_WIDTH] = image.shape[1]
     SHARED_MEM_PARAMS_LIST[ParamsIndex.SCORE_THRESH] = scriptOp.par.Score.eval()
     SHARED_MEM_PARAMS_LIST[ParamsIndex.IOU_THRESH] = scriptOp.par.Nms.eval()
     SHARED_MEM_PARAMS_LIST[ParamsIndex.TOP_K] = scriptOp.par.Maxk.eval()
     SHARED_MEM_PARAMS_LIST[ParamsIndex.ETA] = scriptOp.par.Eta.eval()
-    np.copyto(ARRAY, image)
+    draw_info = DrawInfo.DRAW_BBOX
 
+    if scriptOp.par.Drawtext.eval():
+        draw_info |= DrawInfo.DRAW_TEXT
+
+    if scriptOp.par.Drawscore.eval():
+        draw_info |= DrawInfo.DRAW_CONF
+
+    SHARED_MEM_PARAMS_LIST[ParamsIndex.DRAW_INFO] = int(draw_info)
+
+    ARRAY[:image.shape[0], :image.shape[1]] = image
     SHARED_MEM_UPDATE_STATES.buf[BufferStates.SERVER] = States.READY_SERVER_MESSAGE.value[0]
 
+    start_time = time.monotonic()
+
     while not EXIT and SHARED_MEM_UPDATE_STATES.buf[BufferStates.SERVER_ALIVE] == States.IS_SERVER_ALIVE.value[0] and SHARED_MEM_UPDATE_STATES.buf[BufferStates.CLIENT] != States.READY_CLIENT_MESSAGE.value[0]:
-        pass
+        time.sleep(1e-3)
+        elapsed = time.monotonic() - start_time
+
+        if elapsed > 1:
+            debug("Too long processing copy frame as is")
+            scriptOp.clear()
+            break
 
     if SHARED_MEM_UPDATE_STATES.buf[BufferStates.SERVER_ALIVE] != States.IS_SERVER_ALIVE.value[0]:
         raise ValueError("Server process died")
 
-    scriptOp.copyNumpyArray(ARRAY)
+    scriptOp.copyNumpyArray(ARRAY[:image.shape[0], :image.shape[1]])
     SHARED_MEM_UPDATE_STATES.buf[BufferStates.CLIENT] = States.NULL_STATE.value[0]
-
     return
